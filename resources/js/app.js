eff7fb015fe845333005777b52d18f3c80aa6fd6
Randy Galeano <randyagaleano@gmail.com>
2019-05-24 11:02:13 -0400

    began work on rotating hamburger icon for mobile

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,21 @@
 const hambugerOpen = () => {
     const hamburger = document.querySelector('.hamburger-icon');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        
+        navLinks.forEach((link, index) => {
+            link.style.animation 
+                ? link.style.animation = '' 
+                : link.style.animation = `navFade 0.5s ease forwards ${index / 7 + 0.75}s`;
+        });
+
+        hamburger.classList.toggle('toggle');
+        
     })
+
 }
 
 hambugerOpen();
